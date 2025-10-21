da04e4dc88804e69890a99917b7ac56809ac1eb0
raian2209 <andradeneto1999@gmail.com>
2025-10-20 23:13:07 -0300

    Add CI workflow, Dockerfile, and initial FastAPI application with health check endpoint

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class HealthCheck(BaseModel):
+    status: str = "OK"
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. 
+    """
+    return HealthCheck(status="OK")
